620727d44f3d4b9be10aaa4e5dddf55be369dee9
pravboi <pravachanpatra@gmail.com>
2024-06-14 13:47:48 -0500

    create django project/app

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,17 +37,25 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 👇 Add here your installed app's
+    'rest_framework',
+    'corsheaders',
+    'myapi',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',   # add this
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True   # add this
 
 ROOT_URLCONF = 'myproject.urls'
 
