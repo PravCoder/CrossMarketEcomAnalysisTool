dfdf9ca923fc51a4363875ee6f7e74f6fa446e40
pravboi <pravachanpatra@gmail.com>
2024-06-14 23:17:01 -0500

    Navbar/Login components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div>
+        <Navbar />  
+
         <Routes>
-          
+
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
 
         </Routes>
+        
       </div>
     </Router>
   );
---
+++ frontend/src/components/pages/Login.js
@@ -0,0 +1,12 @@
+import LoginForm from '../LoginForm';
+
+
+function Login() {
+    return (
+      <>
+        <LoginForm />
+      </>
+    );
+  }
+  
+  export default Login;
