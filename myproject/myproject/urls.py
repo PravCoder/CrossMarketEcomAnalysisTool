31fec2ba647ca73ba141f4a616d33089c8fe2795
pravboi <pravachanpatra@gmail.com>
2024-06-19 17:54:41 -0500

    JWT token auth w/ custom user model

--- myproject/myproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # all myapi-app urls will have prefix api/
-    path("api/", include("myapi.urls"))
-]
